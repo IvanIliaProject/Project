f86cd882fd2ac0f1055b4ece6b54164ed0b207a5
IvanIliaProject <56392408+IvanIliaProject@users.noreply.github.com>
2019-12-01 15:03:30 +0200

    LoginController changes

--- src/main/java/JavaFXControllers/LoginController.java
+++ src/main/java/JavaFXControllers/LoginController.java
@@ -2,24 +2,30 @@ package JavaFXControllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import models.UserDetailsEntity;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
+import javax.swing.*;
+import java.io.IOException;
 import java.net.UnknownServiceException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class LoginController {
-   private static UserDetailsEntity currentUser;
-   private static List<UserDetailsEntity> allUsers = new ArrayList<>();
+   public static UserDetailsEntity currentUser;
+   public static List<UserDetailsEntity> allUsers = new ArrayList<>();
 
-   private static List<UserDetailsEntity> loggedUser = new ArrayList<>();
+   public static List<UserDetailsEntity> loggedUser = new ArrayList<>();
 
     @FXML
     public TextField username;
@@ -31,7 +37,7 @@ public class LoginController {
     public Button Register;
     
     @FXML
-    public void createLogin(ActionEvent event) {
+    public void createLogin(ActionEvent event) throws IOException {
         EntityManagerFactory emfactory = Persistence.createEntityManagerFactory("Persistence");
         EntityManager entitymanager = emfactory.createEntityManager();
         entitymanager.getTransaction().begin();
@@ -47,7 +53,14 @@ public class LoginController {
             }
         }
         currentUser = loggedUser.get(loggedUser.size() - 1);
-
+        if (currentUser.getUserTypeEntity().getUserTypeId() == 3){
+        Login.getScene().getWindow().hide();
+        Stage login = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/FxmlFiles/DistributorForm.fxml"));
+        Scene scene = new Scene(root);
+        login.setScene(scene);
+        login.show();
+        }
     }
     @FXML
     public void createRegister(ActionEvent event) {
