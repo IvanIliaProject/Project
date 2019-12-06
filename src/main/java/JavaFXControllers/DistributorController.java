b7348578a58723da3d219c5fa0788c8451c43c94
IvanIliaProject <56392408+IvanIliaProject@users.noreply.github.com>
2019-12-06 14:29:51 +0200

    Add distributor's rating

--- src/main/java/JavaFXControllers/DistributorController.java
+++ src/main/java/JavaFXControllers/DistributorController.java
@@ -1,6 +1,5 @@
 package JavaFXControllers;
 
-import com.jfoenix.controls.JFXListView;
 import com.mysql.jdbc.log.Log;
 import javafx.beans.binding.ObjectExpression;
 import javafx.event.ActionEvent;
@@ -8,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import models.DistributorEntity;
 import models.EventEntity;
+import models.SoldEntity;
 import models.UserDetailsEntity;
 import JavaFXControllers.LoginController;
 import oracle.jdbc.util.Login;
@@ -16,7 +16,10 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import javax.persistence.TypedQuery;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class DistributorController {
@@ -38,16 +41,57 @@ public class DistributorController {
     EntityManager entitymanager = emfactory.createEntityManager( );
 
     List<EventEntity> eventEntities = entitymanager.createQuery("from EventEntity ").getResultList();
-
     @FXML
     private void initialize() {
+            for (EventEntity event : eventEntities) {
+                comboBox.getItems().add(event.getEventName());
+            }
+            CountTicketsCombo.getItems().addAll(1, 2, 3, 5, 6);
+            Date date = new Date();
+            LocalTime localTime = LocalTime.now();
+            entitymanager.getTransaction().begin();
+            List<SoldEntity> solds = new ArrayList<>();
+            List<DistributorEntity> distributorEntityList = new ArrayList<>();
+            solds = entitymanager.createQuery("from SoldEntity ").getResultList();
 
-        for (EventEntity eventEntity : eventEntities) {
-            comboBox.getItems().add(eventEntity.getEventName());
+           /* for (EventEntity entity : eventEntities) {
+                solds = entitymanager.createQuery("from SoldEntity where eventEntity.eventId =: id ")
+                        .setParameter("id",entity.getEventId()).getResultList();
+            }*/
+            for (EventEntity entity : eventEntities) {
+                if (date.after(entity.getEventDate()) && localTime.isAfter(LocalTime.parse(entity.getEventTime()))){
+                    for (SoldEntity sold : solds) {
+                        if (sold.getEventEntity().getEventId() == entity.getEventId()) {
+                            if (sold.getDistributorEntity().getDistributorRating() >= 0 && sold.getDistributorEntity().getDistributorRating() <= 5) {
+                                if (sold.getSold() < 0.2 * entity.getEventSeats() && sold.getDistributorEntity().getDistributorRating() >= 0.2) {
+                                    sold.getDistributorEntity().setDistributorRating(sold.getDistributorEntity().getDistributorRating() - 0.2);
+                                    System.out.println("aaa");
+
+                                    entitymanager.persist(sold);
+
+                                }if (sold.getSold() >= 0.8 * entity.getEventSeats() && sold.getDistributorEntity().getDistributorRating() <= 4.8) {
+                                    sold.getDistributorEntity().setDistributorRating(sold.getDistributorEntity().getDistributorRating() + 0.2);
+                                    System.out.println("bbbb");
+
+                                    entitymanager.persist(sold);
+                                }
+                            }
+                        }
+                        // entityManager.persist(distributorEntity);
+
+                    }
+                    String name = entity.getEventName();
+                    Long id = entity.getEventId();
+
+                    // combobox.getItems().remove(entity.getEventName());
+              /*  entityManager.createQuery("delete from SoldEntity s where s.eventEntity.eventId =: id")
+                        .setParameter("id",id).executeUpdate();
+                entityManager.createQuery("delete from EventEntity e where e.eventName=:name")
+                        .setParameter("name",name).executeUpdate();*/
+                }
+            }
+            entitymanager.getTransaction().commit();
         }
-        CountTicketsCombo.getItems().addAll(1, 2 , 3, 5, 6);
-
-    }
     @FXML
     private double getEventData(ActionEvent event){
         String chosenEvent = comboBox.getSelectionModel().getSelectedItem().toString();
@@ -96,12 +140,12 @@ public class DistributorController {
                 eventEntity  = entity;
             }
         }
-        Integer quantity = eventEntity.getEventQuantity();
-
+        Long quantity = eventEntity.getEventQuantity();
+        double rating = userDetailsEntity.getDistributorEntity().getDistributorRating();
         if (chosenCount<=quantity){
             eventEntity.setEventQuantity(quantity-chosenCount);
-            userDetailsEntity.getDistributorEntity().setDistributorSold(
-                    (userDetailsEntity.getDistributorEntity().getDistributorSold() + chosenCount));
+            /*userDetailsEntity.getDistributorEntity().setDistributorSold(
+                    (userDetailsEntity.getDistributorEntity().getDistributorSold() + chosenCount));*/
             entitymanager.merge(userDetailsEntity);
 
             isSuccess.setVisible(true);
@@ -113,4 +157,5 @@ public class DistributorController {
             isSuccess.setText("No more tickets");
         }
     }
+
 }
