76d722c52e3f01ab2fd85527cf4c5bf831577b27
IvanIliaProject <56392408+IvanIliaProject@users.noreply.github.com>
2019-12-06 14:32:16 +0200

    SignUp validations

--- src/main/java/JavaFXControllers/SignUpController.java
+++ src/main/java/JavaFXControllers/SignUpController.java
@@ -1,11 +1,13 @@
 package JavaFXControllers;
 
+import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -16,6 +18,7 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class SignUpController {
     @FXML
@@ -27,13 +30,19 @@ public class SignUpController {
     @FXML
     public Button login;
     @FXML
-    public TextField Username;
+    public TextField username;
     @FXML
     public TextField PhoneNumber;
+    @FXML
     public Button sign;
+    @FXML
+    public Label isValid;
+    @FXML
+    public Label passwordValidate;
+    @FXML
+    public Label phoneValidate;
 
     @FXML
-
     public void signUp(ActionEvent event) {
 
         final long constTypeId = 4L;
@@ -41,7 +50,7 @@ public class SignUpController {
         String fullnameUser = fullname.getText();
         String passUser = password.getText();
         String emailUser = email.getText();
-        String usernameUser = Username.getText();
+        String usernameUser = username.getText();
         String phoneNumber = PhoneNumber.getText();
 
         EntityManagerFactory emfactory = Persistence.createEntityManagerFactory( "Persistence" );
@@ -50,13 +59,32 @@ public class SignUpController {
         entitymanager.getTransaction( ).begin( );
 
         UserDetailsEntity userDetailsEntity = new UserDetailsEntity();
-        DistributorEntity distributorEntity = new DistributorEntity();
 
+        userDetailsEntity.setUserFullname(fullnameUser);
+        userDetailsEntity.setUserUsername(usernameUser);
+        if(isValidEamil(emailUser) && isValidPassword(passUser) && isValidPhone(phoneNumber)){
+          userDetailsEntity.setUserEmail(emailUser);
+          userDetailsEntity.setUserPhone(phoneNumber);
+          userDetailsEntity.setUserPassword(passUser);
+        UserTypeEntity userTypeEntity = entitymanager.find(UserTypeEntity.class, constTypeId);
+        userDetailsEntity.setUserTypeEntity(userTypeEntity);
         entitymanager.persist(userDetailsEntity);
-        distributorEntity.setDistributorId(userDetailsEntity.getUserId());
-        userDetailsEntity.setDistributorEntity(distributorEntity);
+        }else{
+            if (isValidPassword(passUser)){
+                userDetailsEntity.setUserPassword(passUser);
+            }else{
+                passwordValidate.setVisible(true);
+            }if (isValidPhone(phoneNumber)){
+                userDetailsEntity.setUserPhone(phoneNumber);
+            }else{
+                phoneValidate.setVisible(true);
+            }if (isValidEamil(emailUser)){
+                userDetailsEntity.setUserEmail(emailUser);
+            }else{
+                isValid.setVisible(true);
+            }
+        }
 
-        entitymanager.persist(userDetailsEntity);
         entitymanager.getTransaction().commit();
 
     }
@@ -69,4 +97,34 @@ public class SignUpController {
         login.setScene(scene);
         login.show();
     }
+    public static boolean isValidEamil(String email)
+    {
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\."+
+                "[a-zA-Z0-9_+&*-]+)*@" +
+                "(?:[a-zA-Z0-9-]+\\.)+[a-z" +
+                "A-Z]{2,7}$";
+
+        Pattern pat = Pattern.compile(emailRegex);
+        if (email == null)
+            return false;
+        return pat.matcher(email).matches();
+    }
+   public static boolean isValidPassword(String password){
+        String passwordRegex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+        Pattern pat = Pattern.compile(passwordRegex);
+        if (password == null){
+            return false;
+        }else{
+            return pat.matcher(password).matches();
+        }
+   }
+   public static boolean isValidPhone(String phone){
+        String phoneNumber = "\\d{10,15}";
+        Pattern pat = Pattern.compile(phoneNumber);
+        if (phone == null){
+            return false;
+        }else{
+            return pat.matcher(phone).matches();
+        }
+   }
 }
