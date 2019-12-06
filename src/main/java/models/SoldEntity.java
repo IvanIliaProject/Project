04be825ba76bd0e93adb848e25c9a37a50e82157
IvanIliaProject <56392408+IvanIliaProject@users.noreply.github.com>
2019-12-06 14:34:35 +0200

    SoldEntity

---
+++ src/main/java/models/SoldEntity.java
@@ -0,0 +1,61 @@
+package models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "SOLD", schema = "JAVAPROJECT", catalog = "")
+public class SoldEntity {
+
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "id_Sequence")
+    @SequenceGenerator(name = "id_Sequence", sequenceName = "SOLD_SOLD_ID_SEQ",allocationSize = 1)
+    @Column(name = "SOLD_ID")
+
+    private long soldId;
+
+    public long getSoldId() {
+        return soldId;
+    }
+
+    public void setSoldId(long soldId) {
+        this.soldId = soldId;
+    }
+
+    @Basic
+    @Column(name = "SOLD")
+    private Integer sold;
+
+    public Integer getSold() {
+        return sold;
+    }
+
+    public void setSold(Integer sold) {
+        this.sold = sold;
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY, targetEntity = DistributorEntity.class)
+    @JoinColumn(name = "DISTRIBUTOR_ID")
+    public DistributorEntity distributorEntity;
+
+    public DistributorEntity getDistributorEntity() {
+        return distributorEntity;
+    }
+
+    public void setDistributorEntity(DistributorEntity distributorEntity) {
+        this.distributorEntity = distributorEntity;
+    }
+
+    @ManyToOne(fetch = FetchType.LAZY, targetEntity = EventEntity.class)
+    @JoinColumn(name = "EVENT_ID")
+    public EventEntity eventEntity;
+
+    public EventEntity getEventEntity() {
+        return eventEntity;
+    }
+
+    public void setEventEntity(EventEntity eventEntity) {
+        this.eventEntity = eventEntity;
+    }
+}
